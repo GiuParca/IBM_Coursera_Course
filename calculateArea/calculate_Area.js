ca7cc29cda55602bdd0bc2c49785eb0c61529f10
Giulia <giuliaparca@gmail.com>
2024-07-20 04:40:09 -0400

    grocery tracker

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,10 +1,10 @@
 let length;
 let width;
 
-function calculateArea(){
-    length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value);
-    let area = length * width;
+function calculateArea() {
+ length = parseFloat(document.getElementById('length').value);
+ width = parseFloat(document.getElementById('width').value);
 
-    document.getElementById('result').innerHTML = `The result area of the rectangle is: ${area}`;
+ let area = length * width;
+ document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
