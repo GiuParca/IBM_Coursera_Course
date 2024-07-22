3893bf02f00d5cc03fef02e5682aba7549819bde
Giulia <you@example.comgiuliaparca@gmail.com>
2024-07-22 04:05:53 -0400

    restaurant menu - adding prices

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $42', 'Pasta - $30', 'Burger - $36', 'Salmon - $45'];
+const dessertMenu = ['Cake - $12', 'Ice Cream - $9.80', 'Pudding - $8.60', 'Fruit Salad - $6.70'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index+1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
