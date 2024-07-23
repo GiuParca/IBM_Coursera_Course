51a3d909c29b753a4ed70b9022affb900fae9447
Giulia <giuliaparca@gmail.com>
2024-07-23 09:37:56 -0400

    adding delete button to Book System

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -14,18 +14,20 @@ let books = [];
                 };
                 books.push(book);
                 showbooks();
-                clearInputs();
+                clearInputs();   
             } else {
                 alert('Please fill in all fields correctly.');
             }
-        }
+         };
 
         function showbooks() {
             const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
                 <p><strong>Book Name: </strong>${book.name}</p>
                 <p><strong>Author Name:</strong> ${book.authorName}</p>
                 <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-                <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+                <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+                <button onclick="deleteBook(${index})"> Delete </button>`
+
             );
             document.getElementById('books').innerHTML = booksDiv.join('');
  }
@@ -36,3 +38,8 @@ let books = [];
             document.getElementById('bookDescription').value = '';
             document.getElementById('pagesNumber').value = '';
  }
+
+        function deleteBook(index){
+            books.splice(index,1)
+            showbooks();
+        }
